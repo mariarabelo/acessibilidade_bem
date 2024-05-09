f6c136b2b661b10f624a720ddd1f005ef52ed4ed
mariarabelo <mpaularabelo10@gmail.com>
2024-05-09 11:52:51 -0300

    Criando botao escinder/exibir para o paragrafo

---
+++ script.js
@@ -0,0 +1,12 @@
+$(docuement).ready(function() {
+    $('#botao1').click(function()
+    {
+        $('p').hide('fast');
+    });
+      
+    $('#botao2').click(function()
+    {
+        $('p').show('slow');
+    });
+
+});
